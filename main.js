c18a19bfa7bc75e88978df6a3befac6793e18c80
Djarves <aneresimpelev@mail.ru>
2026-03-14 15:29:07 +0300

    feat: add post creation with title validation and render posts

--- main.js
+++ main.js
@@ -1,25 +1,74 @@
-let title = '';
-let text = '';
-let data = '';
+const posts = [];
 
-const heading = document.querySelector('.heading');
-const titleText = document.querySelector('.title');
-const date = document.querySelector('.date');
-const btn = document.querySelector('.btn');
-const post = document.querySelector('.posts');
+const TITLE_VALIDATION_LIMIT = 100;
+const TEXT_VALIDATION_LIMIT = 100;
 
-btn.addEventListener('click', function() {
-    title = heading.value;
-    text = titleText.value;
-    data = date.value;
-    
-  post.innerHTML = `
-    <div class="post">
-        <h2 class="post-title">${title}</h2>
-        <div class="post-body">${text}</div>
-        <div class="post-date">${data}</div>
-    </div>
-  `;
-  
-     
+const postTitleInputNode = document.querySelector('.js-post-title-input');
+const postTextInputNode = document.querySelector('.js-post-text-input');
+const newPostBtnNode = document.querySelector('.js-new-post-btn');
+const postsNode = document.querySelector('.js-posts');
+const validationMessage = document.getElementById('validationMessage');
+
+
+
+
+newPostBtnNode.addEventListener('click', function() {
+    const postFromUser = getPostFromUser();
+
+    addPost(postFromUser);
+
+    renderPosts();
 });
+
+
+postTitleInputNode.addEventListener('change', function(event) {
+    const currentValue = event.target.value;
+    console.log('change', currentValue, currentValue.length);
+    if (currentValue.length > 100) {
+       validationMessage.innerText = `Длина заголовка не должна превышать ${TITLE_VALIDATION_LIMIT} символов`;
+       validationMessage.classList.remove('validationMessage_hidden');
+    } else {
+       validationMessage.classList.add('validationMessage_hidden'); 
+    }
+});
+
+
+
+function getPostFromUser() {
+    const title = postTitleInputNode.value;
+    const text = postTextInputNode.value;
+
+    return {
+        title: title,
+        text: text,
+    };
+}
+
+function addPost({ title, text }) {
+    posts.push({
+        title: title,
+        text: text
+    });
+}
+
+function getPosts() {
+    return posts;
+}
+
+function renderPosts() {
+    const posts = getPosts();
+
+    let postsHTML = '';
+
+    posts.forEach(post => {
+        postsHTML += `
+        <div class='post'>
+            <p class='post__title'>${post.title}</p>
+            <p class='post__text'>${post.text}</p>
+        </div>
+        `
+    });
+
+
+    postsNode.innerHTML = postsHTML;
+}
